9bdd46a14cac55488287a2870d1dc9052cbf5d34
てんとう虫 <reservecrate@gmail.com>
2026-01-28 13:45:19 +0500

    refactor(demo): move demo page into feature

--- features/Demo/index.ts
+++ features/Demo/index.ts
@@ -2,3 +2,4 @@
 // Public API - only export what's needed by other modules
 
 export { default as DemoGame } from './components/DemoGame';
+export { default as DemoPage } from './components/DemoPage';
